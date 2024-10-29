d5b394879b035ab7b89637f99c604269390c31bc
seishiiin <gabinhalloss@gmail.com>
2024-10-29 16:56:02 +0100

    redesign

--- app/page.js
+++ app/page.js
@@ -1,26 +1,20 @@
-"use client";
-
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
-import { ArrowDownIcon, RocketIcon, GlobeIcon, BackpackIcon, GearIcon, BarChartIcon } from "@radix-ui/react-icons";
-
-import SkillCard from "@/components/SkillCard";
-
 /*
 Palette de couleurs de l'application :
-1.  Arrière-plan de l'application - #11131F
-2.  Arrière-plan subtil - #141726
-3.  Arrière-plan des éléments de l'interface utilisateur - #182449
-4.  Arrière-plan des éléments de l'interface utilisateur survolés - #1D2E62
-5.  Arrière-plan des éléments de l'interface utilisateur actifs / sélectionnés - #253974
-6.  Bordures et séparateurs subtils - #304384
-7.  Bordures des éléments de l'interface utilisateur et anneaux de mise au point - #3A4F97
-8.  Bordure des éléments de l'interface utilisateur survolés - #435DB1
-9.  Arrière-plans solides - #3E63DD
-10. Arrière-plans solides survolés - #5472E4
-11. Texte à faible contraste - #9EB1FF
-12. Texte à fort contraste - #D6E1FF
+    1.  Arrière-plan de l'application - #11131F
+    2.  Arrière-plan subtil - #141726
+    3.  Arrière-plan des éléments de l'interface utilisateur - #182449
+    4.  Arrière-plan des éléments de l'interface utilisateur survolés - #1D2E62
+    5.  Arrière-plan des éléments de l'interface utilisateur actifs / sélectionnés - #253974
+    6.  Bordures et séparateurs subtils - #304384
+    7.  Bordures des éléments de l'interface utilisateur et anneaux de mise au point - #3A4F97
+    8.  Bordure des éléments de l'interface utilisateur survolés - #435DB1
+    9.  Arrière-plans solides - #3E63DD
+    10. Arrière-plans solides survolés - #5472E4
+    11. Texte à faible contraste - #9EB1FF
+    12. Texte à fort contraste - #D6E1FF
 */
 
 export default function Home() {
@@ -28,56 +22,34 @@ export default function Home() {
         <div className={"bg-[#11131F] text-[#EEEEF0] w-dvw min-h-dvh h-auto flex flex-col justify-start items-center"}>
             <Navbar />
 
-            <div className={"relative w-dvw h-[90dvh] flex justify-center items-center sm:px-10 px-5 pb-[10dvh]"}>
-                <div className={"h-full flex flex-col justify-center items-start"}>
-                    <h1 className={"text-5xl font-black"}>Gabin HALLOSSERIE</h1>
-                    <h2 className={"text-2xl"}>Étudiant programmation web</h2>
+            <div className={"relative w-full h-[90dvh] flex justify-between items-center"}>
+                <div className={"h-full sm:px-10 px-5 flex flex-col justify-center items-start gap-6"}>
+                    <h1 className={"relative ml-14 text-[#D6E1FF] text-4xl font-bold"}><span className={"absolute -left-14"}>👋</span>Hey, moi c'est <span className={"text-[#3E63DD]"}>Gabin HALLOSSERIE</span></h1>
 
-                    <div className={"flex justify-start items-center gap-5"}>
-                        <div className={"bg-[#3E63DD] hover:bg-[#5472E4] hover:cursor-alias text-[#D6E1FF] flex justify-start items-center gap-2 rounded-full px-3 py-1 my-5"}>
-                            <BackpackIcon className={"animate-pulse"} />
-                            <p className={"text-sm"}>En recherche d&apos;alternance</p>
+                    <div className={"flex sm:flex-row flex-col sm:justify-start justify-center sm:items-center items-start sm:gap-5 gap-2"}>
+                        <div className={"ml-14 bg-[#182449] flex justify-start items-center gap-2 p-2 rounded-xl"}>
+                            <div className={"bg-[#3E63DD] w-[10px] h-[10px] rounded-full animate-pulse"}></div>
+                            <h2 className={"text-[#3E63DD] font-bold  text-sm"}>À la recherche d'une alternance</h2>
                         </div>
 
-                        <div className={"bg-red-500 hover:bg-red-600 hover:cursor-alias text-[#D6E1FF] flex justify-start items-center gap-2 rounded-full px-3 py-1 my-5"}>
-                            <RocketIcon className={"animate-pulse"} />
-                            <p className={"text-sm"}>Portfolio en cours de construction</p>
+                        <div className={"ml-14 sm:ml-0 bg-orange-100 flex justify-start items-center gap-2 p-2 rounded-xl"}>
+                            <div className={"bg-orange-900 w-[10px] h-[10px] rounded-full animate-pulse"}></div>
+                            <h2 className={"text-orange-900 font-bold  text-sm"}>Portfolio en cours de création</h2>
                         </div>
                     </div>
 
-                    <p className={"w-5/6 text-base"}>Actuellement à l&apos;ESN 81 à Castres, je suis étudiant en programmation logiciel et web. Je suis à la recherche d&apos;un contrat d&apos;apprentissage pour une durée de 1 à 3 ans.</p>
-
+                    <p className={"w-3/5 ml-14 text-[#D6E1FF] text-sm"}>Je suis un étudiant en programmation web passionné par le développement d'applications web modernes et performantes. J'aime apprendre de nouvelles technologies et les appliquer dans des projets personnels. Je suis actuellement en bachelor CSI à l&apos;ESN 81 à Castres</p>
                 </div>
 
-                <div className={"w-full h-full lg:flex hidden flex-col justify-center items-end"}>
-                    <Image src={"/assets/webstorm.png"} alt={"Photo de profil"} width={3840} height={2160} className={"xl:w-[calc(1920px/3.5)] w-[calc(1920px/5)] xl:h-[calc(1080px/3.5)] h-[calc(1080px/5)] rounded-md"} />
+                <div className={"hidden xl:flex h-full sm:px-10 px-5 flex-col justify-center items-end"}>
+                    <Image src={"/assets/webstorm.png"} alt={"Photo de mon environnement de développement"} width={1500} height={844} />
                 </div>
 
-                <div className={"absolute bottom-4 w-dvw h-[5dvh] flex justify-center items-center"}>
-                    <a href={"#about"} className={"flex justify-center items-center gap-2 animate-bounce"}>
-                        <span className={"text-sm"}><ArrowDownIcon /></span>
-                        <span className={"text-sm"}>En savoir plus</span>
-                        <span className={"text-sm"}><ArrowDownIcon /></span>
-                    </a>
-                </div>
-            </div>
-
-            <div id={"about"} className={"w-dvw h-[90dvh] flex justify-between items-end py-[10dvh] sm:px-10 px-5"}>
-                <SkillCard middle={false}
-                           title={"Développement logiciel"}
-                           description={"Maîtrise de Java et Python, avec quelques notions sur Windev pour le développement d'applications."}
-                            icon={<GearIcon className={"w-12 h-12 text-[#D6E1FF]"}/>}
-                />
-                <SkillCard middle={true}
-                           title={"Développement web"}
-                           description={"Création de sites web dynamiques avec HTML, CSS, PHP et JavaScript. Expérience avec les frameworks CodeIgniter, Next.js et les ORM Prisma, Eloquent."}
-                           icon={<GlobeIcon className={"w-12 h-12 text-[#D6E1FF]"}/>}
-                />
-                <SkillCard middle={false}
-                           title={"Base de données"}
-                           description={"Gestion de bases de données avec MySQL. Conception MCD et requêtes SQL optimisées."}
-                            icon={<BarChartIcon className={"w-12 h-12 text-[#D6E1FF]"}/>}
-                />
+               {/*<a href={"#"} className={"absolute w-[300px] text-center bottom-10 left-[calc(50%-150px)] transform -translate-x-1/2 flex justify-center items-center gap-5 animate-bounce"}>
+                    <DoubleArrowDownIcon className={"w-4 h-4 text-[#3E63DD]"} />
+                    <p className={"text-[#D6E1FF] text-sm"}>En savoir plus sur moi</p>
+                    <DoubleArrowDownIcon className={"w-4 h-4 text-[#3E63DD]"} />
+                </a>*/}
             </div>
         </div>
     );
